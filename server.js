45f926772d804896a494278d9d022c4e8859a352
Holliant <85192503+Holliant@users.noreply.github.com>
2022-01-15 18:32:23 +0000

    add start up cmd

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const app = express()
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    console.log("Here")
-    res.send("Hi")
-})
+// app.get('/', (req, res) => {
+//     console.log("Here")
+//     res.send("Hi")
+// })
 
 app.get('/lotto', (req, res) => {
-    console.log("Here")
-    res.send("Lotto page here")
+    res.render("lotto", {});
 })
 
 app.listen(3000)
