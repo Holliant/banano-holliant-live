bc134748cdffa1bf90f2332c9d8fc5fdf1452e58
Shay <sgaming24@outlook.com>
2022-01-15 22:49:29 +0000

    Add 404 error

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-require(`dotenv`).config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-// app.get('/', (req, res) => {
-//     console.log("Here");
-//     res.send("Hi");
+// app.get("/", (req, res) => {
+//     res.render("index", {});
 // })
-
-app.get('/lotto', (req, res) => {
+    
+app.get("/lotto", (req, res) => {
     res.render("lotto", {});
 })
 
+app.get("*", (req, res, next) => {
+    res.json({
+        "code": 404,
+        "error": "Page not found"
+    });
+    next();
+})
+    
 app.listen(process.env["PORT"] || 3000);
