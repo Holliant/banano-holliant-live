ed9fce37b42568b07926a934503a4a2f33c46377
Shay <sgaming24@outlook.com>
2022-01-15 18:35:56 +0000

    Use heroku-provided port

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
+require(`dotenv`).config();
+
+const express = require('express');
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 // app.get('/', (req, res) => {
-//     console.log("Here")
-//     res.send("Hi")
+//     console.log("Here");
+//     res.send("Hi");
 // })
 
 app.get('/lotto', (req, res) => {
     res.render("lotto", {});
 })
 
-app.listen(3000)
+app.listen(process.env["PORT"]);
