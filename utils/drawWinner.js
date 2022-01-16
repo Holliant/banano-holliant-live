eaabf4ff5d32c224ada7de3fda9059a37d1fc5eb
Shay <sgaming24@outlook.com>
2022-01-16 18:50:53 +0000

    Full jackpot backend

--- utils/drawWinner.js
+++ utils/drawWinner.js
@@ -12,9 +12,16 @@ let keys = [privateKey];
 
 // const privateKey = bananojs.getPrivateKey(privateSeed, 0);
 
-bananojs.getPublicKey(privateKey).then(pubKey => console.log(`Send funds to ${bananojs.getBananoAccount(pubKey)}`));
+let lottoAddress = "";
 
-let sendBan = async (destAccount, amountRaw, pkI=0) => {
+const setup = async () => {
+    await bananojs.getPublicKey(privateKey).then(publicKey => {
+        lottoAddress = bananojs.getBananoAccount(publicKey);
+    });
+    console.log(`Send funds to ${lottoAddress}`);
+}
+
+const sendBan = async (destAccount, amountRaw, pkI=0) => {
     const response = await bananojs.bananoUtil.sendFromPrivateKey(
         bananojs.bananodeApi,
         keys[pkI],
@@ -24,9 +31,16 @@ let sendBan = async (destAccount, amountRaw, pkI=0) => {
     );
     console.log(`payment successful: (${(amountRaw / 1e29).toFixed(8)} BAN)`, response);
     return response;
+}
+
+const accountInfo = async (account) => {
+    let accInfo = await bananojs.getAccountInfo(account, {
+        "representative": "true"
+    });
+    return accInfo;
 };
 
-let getBlockInfo = async (blockHash) => {
+const getBlockInfo = async (blockHash) => {
     let toReturn = {};
     await axios.post(config["http-node"], {  
         "action": "block_info",
@@ -40,7 +54,66 @@ let getBlockInfo = async (blockHash) => {
     return toReturn;
 }
 
-let recvPending = async (pkI=0) => {
+const listPending = async (address) => {
+    let toReturn = {};
+    await axios.post(config["http-node"], {
+        "action": "accounts_pending",
+        "accounts": [address],
+        "threshold": config["ticket-price"].toLocaleString("fullwide", { useGrouping: false }),
+        "source": true
+    })
+    .then(res => {
+        toReturn = res.data;
+    })
+    .catch(err => console.log(err));
+    return toReturn;
+}
+
+const receivePending = async (pkI=0) => {
     const txList = await bananojs.receiveBananoDepositsForSeed(privateSeed, pkI, config["rep-account"]);
-    console.log(txList);
-};
+}
+
+const pickWinner = async () => {
+    let lottoEntries = [];
+    await listPending(lottoAddress).then(res => {
+        for (const [txHash, txInfo] of Object.entries(res["blocks"][lottoAddress])) {
+            for (
+                i = 0;
+                i < Math.floor(txInfo["amount"] / config["ticket-price"]);
+                i++
+            ) {
+                lottoEntries.push(txInfo["source"]);
+            };
+        };
+    });
+    return [lottoEntries[Math.floor(Math.random() * lottoEntries.length)], lottoEntries];
+}
+
+const lottoDraw = async () => {
+    let winnerAddress;
+    let totalEntries;
+    let winnerTickets = 0;
+    await pickWinner().then(res => {
+        winnerAddress = res[0];
+        totalEntries = res[1].length;
+        res[1].forEach(userAddress => {
+            if (userAddress == winnerAddress) winnerTickets += 1;
+        });
+    });
+    await receivePending();
+    let winAmount = (await accountInfo(lottoAddress)).balance;
+    let payoutHash = await sendBan(winnerAddress, winAmount);
+    return {
+        "address": winnerAddress,
+        "tickets": winnerTickets,
+        "total": totalEntries,
+        "hash": payoutHash
+    };
+}
+
+(async () => {
+    await setup();
+    lottoDraw().then(res => console.log(res));
+})();
+
+module.exports = lottoDraw;
