aedd5f27cdd2f20c791abf17f39058e3615c9971
Shay <sgaming24@outlook.com>
2022-01-18 14:39:38 +0000

    Add previous winner info

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const config = require(`./config.json`);
 
 const express = require("express");
 const app = express();
@@ -11,14 +12,24 @@ app.set("view engine", "ejs");
 // })
     
 app.get("/lotto", (req, res) => {
-    res.render("lotto", {});
+    res.render("lotto", {
+        "ticketPrice": config["ticket-price"],
+        "jackpotAmount": 314,
+        "lottoEnd": Date.now() + 60000,
+        "previousWinner": {
+            "address": "ban_3freefkgfnazw7ajt1xyqgmijsjb9oc3mykm18pewy11wd7twd5hf4cbxekh",
+            "hash": "F136DD03A2E4E39EF642ADF8551D6B74868D1B6BD3F647B912176AB7F06A6EFC",
+            "amount": 10.52
+        }
+    });
 })
 
 app.get("*", (req, res, next) => {
-    res.json({
-        "code": 404,
-        "error": "Page not found"
-    });
+    // res.json({
+    //     "code": 404,
+    //     "error": "Page not found"
+    // });
+    res.redirect("/lotto");
     next();
 })
     
