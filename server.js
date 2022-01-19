6d8d79d2106f33227eca962a0e5b16920e6bbe20
Shay <sgaming24@outlook.com>
2022-01-19 11:57:53 +0000

    Previous winner checking logic

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const app = express();
 const lottoUtils = require("./utils/lottoUtils.js");
 
 let lottoAddress = "";
+let previousWinner = {
+    "address": "ban_3freefkgfnazw7ajt1xyqgmijsjb9oc3mykm18pewy11wd7twd5hf4cbxekh",
+    "hash": "F136DD03A2E4E39EF642ADF8551D6B74868D1B6BD3F647B912176AB7F06A6EFC",
+    "amount": 10.52
+};
 
 lottoUtils.getLottoAddress().then(res => {
-    console.log(res);
     lottoAddress = res;
 });
 
@@ -21,11 +25,7 @@ app.get("/lotto", (req, res) => {
         "ticketPrice": config["ticket-price"],
         "jackpotAmount": 314,
         "lottoEnd": Date.now() + 60000,
-        "previousWinner": {
-            "address": "ban_3freefkgfnazw7ajt1xyqgmijsjb9oc3mykm18pewy11wd7twd5hf4cbxekh",
-            "hash": "F136DD03A2E4E39EF642ADF8551D6B74868D1B6BD3F647B912176AB7F06A6EFC",
-            "amount": 10.52
-        },
+        "previousWinner": previousWinner,
         "lottoAddress": lottoAddress
     });
 })
